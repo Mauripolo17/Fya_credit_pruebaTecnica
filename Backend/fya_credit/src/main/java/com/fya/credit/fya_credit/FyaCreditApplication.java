2667d53586171e4186882e156b8344d8f8e8718c
Mauricio Polo <142242506+Mauripolo17@users.noreply.github.com>
2025-08-11 10:48:46 -0500

    Initial commit

---
+++ Backend/fya_credit/src/main/java/com/fya/credit/fya_credit/FyaCreditApplication.java
@@ -0,0 +1,13 @@
+package com.fya.credit.fya_credit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyaCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FyaCreditApplication.class, args);
+	}
+
+}
